03c95033f98c5d7f898bc11f1e37055f175aef93
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2022-11-03 17:19:12 +0530

    add initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Navbar from "./Layout/Navbar";
+
+const defaultCode = `
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  cout << "Hello World";
+  return 0;
+}
+`;
 
 function App() {
+  const [submission, setSubmission] = useState(defaultCode);
+  const [token, setToken] = useState(null);
+  const [output, setOutput] = useState(null);
+
+  const judgeUrl = "https://judge0-ce.p.rapidapi.com";
+
+  const getDataHandler = async () => {
+    if (token) {
+      const options2 = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_X_RapidAPI_KEY,
+          "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+        },
+      };
+
+      fetch(
+        `${judgeUrl}/submissions/${token}?base64_encoded=true&fields=*`,
+        options2
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          setOutput(atob(response.stdout));
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+
+  useEffect(() => {
+    getDataHandler();
+  }, [token]);
+
+  const submissionHandler = async () => {
+    const submissionData = {
+      language_id: 54,
+      source_code: btoa(submission),
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+        "X-RapidAPI-Key": process.env.REACT_APP_X_RapidAPI_KEY,
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+      },
+      body: JSON.stringify(submissionData),
+    };
+
+    fetch(`${judgeUrl}/submissions?base64_encoded=true&fields=*`, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+
+        if (response.token) {
+          setToken(response.token);
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-start w-full">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          value={submission}
+          className="bg-black text-orange-600 caret-white w-4/5 code resize-x"
+          onChange={(e) => {
+            setSubmission(e.target.value);
+          }}
+          autoFocus
+        />
+        <button
+          onClick={() => {
+            submissionHandler();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </div>
+      {token && <h1>{token}</h1>}
+      {output && <h1>{output}</h1>}
     </div>
   );
 }
---
+++ src/Layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row items-center justify-between sticky top-0 bg-rose-600 text-white text-2xl py-2 px-4 ">
+      Code Compiler
+    </div>
+  );
+};
+
+export default Navbar;
